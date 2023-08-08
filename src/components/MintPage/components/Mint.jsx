72a8a947e9cc08742637f2fa5bed9a8e0f699e13
Tswap007 <tswap700@gmail.com>
2023-08-08 14:43:44 +0100

    Gotten how to upload the image ang get MetaData. Next is to add restriction that prevents minting if all the traits have not yet been selectes. And I start working on the contract next.

--- src/components/MintPage/components/Mint.jsx
+++ src/components/MintPage/components/Mint.jsx
@@ -1,8 +1,10 @@
 import { Center, Button } from "@chakra-ui/react";
 import { checkIfAnyTraitSelected } from "./Selected";
+import { NFTStorage, File } from "nft.storage";
 
 
-
+const NFT_STORAGE_TOKEN = import.meta.env.VITE_NFTSTORAGE_API_KEY;
+const client = new NFTStorage({ token: NFT_STORAGE_TOKEN })
 
 export default function MintButton({ selectedTraits, stageRef }) {
 
@@ -10,16 +12,58 @@ export default function MintButton({ selectedTraits, stageRef }) {
 
     async function getImageUrl() {
         const dataURL = await stageRef.current.toDataURL();
-        for (let i = 0; i < selectedTraits.length; i++) {
-            console.log(selectedTraits[i].label);
-        }
+        // console.log(dataURL);
         return dataURL;
     };
 
-    async function uploadImageUrl() {
-        imageUrl = await (getImageUrl());
+    async function uploadImageAndGetMetadata() {
+        console.log("Getting Image from Canvas")
+        const imageUrl = await getImageUrl();
 
+        // Convert the base64 image data to a Blob
+        console.log("Converting Image to blob")
+        const imageBlob = await fetch(imageUrl).then(response => response.blob());
+
+        // Create a File object from the image Blob
+        const imageFile = new File([imageBlob], 'wanderer.png', { type: 'image/png' });
+
+        // Use the NFTStorage client to store the image and generate metadata
+        console.log("Uploading Image and getting MetaData")
+        const metadata = await client.store({
+            name: 'Wanderer',
+            description: 'Who says that this isn\'t fun',
+            image: imageFile,
+            attributes: [
+                {
+                    "trait_type": "Background",
+                    "value": `${selectedTraits[0].label}`
+                },
+                {
+                    "trait_type": "Body",
+                    "value": `${selectedTraits[1].label}`
+                },
+                {
+                    "trait_type": "Head",
+                    "value": `${selectedTraits[2].label}`
+                },
+                {
+                    "trait_type": "Expression",
+                    "value": `${selectedTraits[3].label}`
+                },
+                {
+                    "trait_type": "Pet",
+                    "value": `${selectedTraits[4].label}`
+                }
+            ]
+        });
+        console.log("Upload Complete")
+        console.log(metadata)
+        return metadata;
     }
+    function seeLength() {
+        console.log(selectedTraits.length)
+    }
+
 
     return (
         shouldRenderList ? (
@@ -30,7 +74,8 @@ export default function MintButton({ selectedTraits, stageRef }) {
                     mb={3}
                     borderRadius="24px"
                     boxShadow="6px 7px 0px 0px rgba(0, 0, 0, 0.8)"
-                    onClick={getImageUrl}
+                    // isDisabled={!selectedTraits.length !== 5}
+                    onClick={seeLength}
                 >
                     MINT
                 </Button>
