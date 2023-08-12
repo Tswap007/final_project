d6358e1b14526f2753585a171d9cf20b47f0251a
Tswap007 <tswap700@gmail.com>
2023-08-12 18:03:21 +0100

    Finally solved the mint bug after 4 days man!!! Used the wagmis core actions for usePrepareWriteContract and useWriteContract as the hook usePrepareWriteContract had issues collecting dynamic args and updating it before mounting. This is my longest time spent on a bug yet new record for me I guess LOL. Next order of business is to have feedback for the user through out the minting process.

--- src/components/MintPage/components/Mint.jsx
+++ src/components/MintPage/components/Mint.jsx
@@ -1,21 +1,27 @@
-import { Center, Button, Tooltip, Box } from "@chakra-ui/react";
+// Import required modules and components
+import React, { useEffect, useState } from "react";
+import { Center, Button, Tooltip } from "@chakra-ui/react";
 import { checkIfAnyTraitSelected } from "./Selected";
 import { NFTStorage, File } from "nft.storage";
-import { useEffect, useState } from "react";
-import { usePrepareContractWrite, useContractWrite, useWaitForTransaction, useAccount } from "wagmi";
+import { useAccount } from "wagmi";
+import { prepareWriteContract, writeContract } from "wagmi/actions"
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 
-
+// Initialize NFT Storage client
 const NFT_STORAGE_TOKEN = import.meta.env.VITE_NFTSTORAGE_API_KEY;
-const client = new NFTStorage({ token: NFT_STORAGE_TOKEN })
+const client = new NFTStorage({ token: NFT_STORAGE_TOKEN });
 
+// Smart contract address
+const ContractAddress = '0xf81352C5Cdd5665AB735CEf0947e2EF3230F0bC5';
+
+// Define the MintButton component
 export default function MintButton({ selectedTraits, stageRef }) {
-
+    // Calculate remaining traits to select and button disable status
     const shouldRenderList = checkIfAnyTraitSelected(selectedTraits);
     const [isButtonDisabled, setIsButtonDisabled] = useState(true);
     const [numberLeft, setNumberLeft] = useState(5);
-    const [metaDataURI, setMetaDataURI] = useState("");
 
+    // Update UI when selected traits change
     useEffect(() => {
         let count = 0;
         for (let i = 0; i < selectedTraits.length; i++) {
@@ -28,134 +34,104 @@ export default function MintButton({ selectedTraits, stageRef }) {
         setIsButtonDisabled(count !== 5);
     }, [selectedTraits]);
 
-
+    // Function to get image URL from canvas
     async function getImageUrl() {
         const dataURL = await stageRef.current.toDataURL();
-        // console.log(dataURL);
         return dataURL;
-    };
+    }
 
+    // Function to upload metadata to IPFS and return URI
     async function uploadMetadataToIPFSAndReturnURI() {
-        console.log("Getting Image from Canvas");
         const imageUrl = await getImageUrl();
-
-        // Convert the base64 image data to a Blob
-        console.log("Converting Image to blob");
-        const imageBlob = await fetch(imageUrl).then(response => response.blob());
-
-        // Create a File object from the image Blob
+        const imageBlob = await fetch(imageUrl).then((response) => response.blob());
         const imageFile = new File([imageBlob], 'wanderer.png', { type: 'image/png' });
 
-        // Use the NFTStorage client to store the image and generate metadata
-        console.log("Uploading Image and getting Metadata");
         const metadata = await client.store({
             name: 'Wanderer',
             description: 'Who says that this isn\'t fun',
             image: imageFile,
             attributes: [
-                {
-                    "trait_type": "Background",
-                    "value": `${selectedTraits[0].label}`
-                },
-                {
-                    "trait_type": "Body",
-                    "value": `${selectedTraits[1].label}`
-                },
-                {
-                    "trait_type": "Head",
-                    "value": `${selectedTraits[2].label}`
-                },
-                {
-                    "trait_type": "Expression",
-                    "value": `${selectedTraits[3].label}`
-                },
-                {
-                    "trait_type": "Pet",
-                    "value": `${selectedTraits[4].label}`
-                }
+                // Define attributes based on selected traits
+                { "trait_type": "Background", "value": `${selectedTraits[0].label}` },
+                { "trait_type": "Body", "value": `${selectedTraits[1].label}` },
+                { "trait_type": "Head", "value": `${selectedTraits[2].label}` },
+                { "trait_type": "Expression", "value": `${selectedTraits[3].label}` },
+                { "trait_type": "Pet", "value": `${selectedTraits[4].label}` }
             ]
         });
 
-        //store metaData to IPFS
-        const metaDataUploadResponse = await client.storeBlob(new Blob([JSON.stringify(metadata)]));
-        console.log("Metadata Upload Complete");
-        const cid = metaDataUploadResponse; // The CID returned from IPFS
-
-        // Construct the metadata URI using the nftStorage gateway URL and the CID
-        const metadataUrl = `$https://nftstorage.link/ipfs/${cid}`;
-        console.log("Metadata URI:", metadataUrl);
-
-        return metadataUrl;
+        const metaDataURL = metadata.url;
+        console.log(metaDataURL);
+        return metaDataURL;
     }
 
-    // mint function is giving me a tough time but I shall prevail LOL..
+    // Fetch user account information
     const { isConnected, address } = useAccount();
-    const { config } = usePrepareContractWrite({
-        address: '0xf81352C5Cdd5665AB735CEf0947e2EF3230F0bC5',
-        abi: [
-            {
-                "inputs": [
-                    {
-                        "internalType": "address",
-                        "name": "to",
-                        "type": "address"
-                    },
-                    {
-                        "internalType": "string",
-                        "name": "uri",
-                        "type": "string"
-                    }
-                ],
-                "name": "safeMint",
-                "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function"
-            },
-        ],
-        functionName: 'safeMint',
-        args: [address, metaDataURI]
-    })
-    const { data, write } = useContractWrite(config);
-    const { isLoading, isSuccess } = useWaitForTransaction({
-        hash: data?.hash,
-    });
-    if (isLoading) {
-        console.log("Transaction Loading")
-    }
-    if (isSuccess) {
-        console.log("TX succesful")
+
+    // Prepare contract write configuration
+    async function prepareContractWrite(metaDataURL) {
+        const config = await prepareWriteContract({
+            address: ContractAddress,
+            abi: [
+                {
+                    "inputs": [
+                        { "internalType": "address", "name": "to", "type": "address" },
+                        { "internalType": "string", "name": "uri", "type": "string" }
+                    ],
+                    "name": "safeMint",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+            ],
+            functionName: 'safeMint',
+            args: [address, metaDataURL],
+        });
+
+        return config;
     }
 
+    // Write mint contract
+    async function writeMintContract(request) {
+        const { hash } = await writeContract(request);
+        return hash;
+    }
+
+    // Open connection modal
     const { openConnectModal } = useConnectModal();
 
+    // Handle mint button click
     const handleMintButtonClick = async () => {
         if (isConnected) {
-            const metaDataURI = await uploadMetadataToIPFSAndReturnURI();
-            setMetaDataURI(metaDataURI);
-            console.log(metaDataURI)
-            write();
+            const metaDataURL = await uploadMetadataToIPFSAndReturnURI();
+            const request = await prepareContractWrite(metaDataURL);
+            const hash = await writeMintContract(request);
+            console.log(hash);
+        } else {
+            openConnectModal();
         }
-        else { openConnectModal() };
     };
 
-
-    return (
-        shouldRenderList ? (
-            <Center>
-                <Tooltip label={`Select ${numberLeft} more trait${numberLeft === 1 ? "" : "s"} To Proceed`} isOpen={isButtonDisabled} hasArrow>
-                    <Button
-                        width="70%"
-                        background="#5FC95D"
-                        mb={3}
-                        borderRadius="24px"
-                        boxShadow="6px 7px 0px 0px rgba(0, 0, 0, 0.8)"
-                        isDisabled={isButtonDisabled || isLoading}
-                        onClick={handleMintButtonClick}
-                    >
-                        {isLoading ? "Minting..." : "Mint"}
-                    </Button>
-                </Tooltip>
-            </Center>
-        ) : null
-    );
+    // Render MintButton component
+    return shouldRenderList ? (
+        <Center>
+            <Tooltip
+                label={`Select ${numberLeft} more trait${numberLeft === 1 ? "" : "s"} To Proceed`}
+                isOpen={isButtonDisabled}
+                hasArrow
+            >
+                <Button
+                    width="70%"
+                    background="#5FC95D"
+                    mb={3}
+                    borderRadius="24px"
+                    boxShadow="6px 7px 0px 0px rgba(0, 0, 0, 0.8)"
+                    isDisabled={isButtonDisabled}
+                    onClick={handleMintButtonClick}
+                >
+                    Mint
+                </Button>
+            </Tooltip>
+        </Center>
+    ) : null;
 }
